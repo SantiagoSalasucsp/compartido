fdf93ebfc5a215ebcb90fa081d853a0f484c4f2d
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2024-11-20 15:28:15 -0500

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -2,23 +2,23 @@ import sysv_ipc
 import time
 import sys
 
-KEY = 0x1111      # key del semaforo
-SHM_KEY = 5678    # key shm
-SHM_SIZE = 27   # size shm
+KEY = 0x1111      
+SHM_KEY = 5678    
+SHM_SIZE = 27   
 
 def px_python(text, V):
     try:
-        # acceder al semaforo
+        
         semaphore = sysv_ipc.Semaphore(KEY)
 
-        # acceder shm
+       
         shared_memory = sysv_ipc.SharedMemory(SHM_KEY,flags=sysv_ipc.IPC_CREAT,mode=0o666,size=27)
 
         while True:
-            # p
+           
             semaphore.acquire()
 
-            # leer los 3 primeros bytes
+            
             shmv = int(shared_memory.read(3).decode('utf-8'))
 
             if shmv == (V - 1):
@@ -29,7 +29,7 @@ def px_python(text, V):
                 
                 V += 4
 
-            # v
+           
             semaphore.release()
             time.sleep(1)
 
